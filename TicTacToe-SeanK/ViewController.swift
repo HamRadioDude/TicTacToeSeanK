ba0570b525e0d43b5cfaa54bd3689435aca664ad
Sean Klechak <sklechak@students.mchenry.edu>
2019-02-10 11:28:11 -0600

    Initial Commit

---
+++ TicTacToe-SeanK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TicTacToe-SeanK
+//
+//  Created by student on 2/10/19.
+//  Copyright © 2019 Sean Klechak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
